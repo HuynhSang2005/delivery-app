fdd0fb93ed2617feceb3ea2fec345c3e6c03e12d
Huỳnh Sang <huynhsang060305@gmail.com>
2026-03-26 23:24:21 +0700

    chore(api): migrate package-manager npm to bun

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+function bootstrap(): void {
+  NestFactory.create(AppModule)
+    .then((app) => app.listen(process.env.PORT ?? 3000))
+    .catch((error: unknown) => {
+      throw error;
+    });
 }
+
 bootstrap();
